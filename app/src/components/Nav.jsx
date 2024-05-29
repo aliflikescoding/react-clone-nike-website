3555641dd6dc9e37b0e74b97bb2f51602f63c83e
Alif Wibowo <alif.likes.coding@gmail.com>
2024-05-30 05:10:55 +0700

    Move constants again

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,7 +1,23 @@
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../assets/constants";
+
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => {
+            <li key={item.label}>
+              <a href={item.href}></a>
+            </li>
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
