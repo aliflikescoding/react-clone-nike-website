05389e592fecdb3e26e4cc6ed93a73cc8380b964
Alif Wibowo <alif.likes.coding@gmail.com>
2024-05-31 19:03:13 +0700

    Modify main app styles

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,36 +1,36 @@
 import { Hero, PopularProducs, SuperQuality, Services, SpecialOffers, CustomersReviws, Subscribe, Footer } from './sections';
 import Nav from './components/Nav';
 
-const App = () => (
-  <main className="relative">
-    <Nav />
-    <section>
-      <section className="xl:padding-1 wide:padding-r paddiing-b">
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <PopularProducs />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SuperQuality />
       </section>
-      <section className="padding-x">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SpecialOffers />
       </section>
-      <section className="bg-pale-blue padding">
+      <section className='bg-pale-blue padding'>
         <CustomersReviws />
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="bg-black padding-x padding-t pb-8">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
-    </section>
-  </main>
-);
+    </main>
+  );
+};
 
 export default App
